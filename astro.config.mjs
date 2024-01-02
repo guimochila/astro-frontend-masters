64b27c14e91deadfead363e3ffdae0189fa93143
Guilherme <gui.mochila@gmail.com>
2024-01-02 14:25:35 +0100

    feat: Course completed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
 import { defineConfig } from 'astro/config';
 
+import react from "@astrojs/react";
+import solidJs from "@astrojs/solid-js";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://guimochila.com',
+  output: 'hybrid',
+  experimental: {
+    hybridOutput: true
+  },
+  integrations: [react(), solidJs()]
+});
